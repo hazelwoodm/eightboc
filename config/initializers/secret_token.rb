8083518dd174b0da8983901140c9d3d0f451121d
hazelwoodm <hazelwoodm@goldmail.etsu.edu>
2012-05-29 14:27:30 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eightboc::Application.config.secret_token = '96249430e5dbd752808967f830442724fc083a125da55b9469b3b63df4ffec85a07de8d1132179383536a43b92ce353ce452002daf33a457128cae00e643149e'
